208b0a59545f648c278fcdc6c559803446a697f6
DariaCode <54681621+DariaCode@users.noreply.github.com>
2020-04-30 16:02:52 -0400

    added feature to create a repeating task

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -23,7 +23,7 @@ module.exports = (req, res, next) => {
   }
   let decodedToken;
   try {
-    decodedToken = jwt.verify(token, 'willyisthebestdog');
+    decodedToken = jwt.verify(token, 'somesecretword');
   } catch (err) {
     req.isAuth = false;
     return next();
